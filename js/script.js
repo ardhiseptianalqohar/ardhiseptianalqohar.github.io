d401fe0b96103be3f6b1894c4c1dff51c0add439
ardhiseptianalqohar <ardhiseptianalqohar@gmail.com>
2022-10-28 15:36:30 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
-yaakovGreeter.sayHello();
-johnGreeter.sayHi();
 
-// Immediately Invoked Function Expression
-// IIFE
-(function (name) {
-  console.log("Hello " + name);
-})("Coursera!");
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
+  for (var i = 0; i < names.length; i++) {
+
+    var firstLetter = names[i].charAt(0);
+    firstLetter = firstLetter.toLowerCase();
+
+    if (firstLetter == "j") {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
 
 
 
